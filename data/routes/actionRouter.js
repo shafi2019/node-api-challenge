08e088b2643583554bf0eaf4e13f6ecb557b4d73
Shafi Masoumi <masoumi2017@icloud.com>
2020-01-25 11:45:25 -0800

    action CRUD done

--- data/routes/actionRouter.js
+++ data/routes/actionRouter.js
@@ -14,4 +14,43 @@ router.get('/', (req, res) => {
     })
 })
 
+
+router.post('/:id', (req, res) => {
+    actions.insert(req.id)
+    .then(action => {
+        res.status(201).json(action);
+    })
+    .catch(error => {
+        res.status(404).json({
+            message: "can not be added"
+        })
+    })
+});
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    actions.update(id, body)
+    .then(action => {
+        res.status(200),json(action)
+    })
+    .catch(error => {
+        res.status(404).json({
+            errorMessage: "Can not Update Action", error
+        })
+    })
+});
+
+router.delete("/:id", (req, res) => {
+    actions.remove(req.params.id)
+    .then(action => {
+        res.status(200).json(action)
+    })
+    .catch(error => {
+        res.status(500).json({
+            errorMessage:`could not delete action by id`, error
+        })
+    })
+});
+
 module.exports = router;
