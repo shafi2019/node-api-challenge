012bedd5597ffb660bee4daeed078f7d15bcd3e2
Shafi Masoumi <masoumi2017@icloud.com>
2020-01-26 09:08:41 -0800

    updates

--- data/routes/actionRouter.js
+++ data/routes/actionRouter.js
@@ -4,53 +4,55 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     actions.get()
-    .then(data => {
-        res.status(200).json(data)
-    })
-    .catch(error => {
-        res.status(500).json({ 
-            message: "Did not find the action", error
+        .then(data => {
+            res.status(200).json(data)
         })
-    })
+        .catch(error => {
+            res.status(500).json({
+                message: "Did not find the action", error
+            })
+        })
 })
 
 
 router.post('/:id', (req, res) => {
     actions.insert(req.id)
-    .then(action => {
-        res.status(201).json(action);
-    })
-    .catch(error => {
-        res.status(404).json({
-            message: "can not add action", error
+        .then(action => {
+            res.status(201).json(action);
         })
-    })
+        .catch(error => {
+            res.status(404).json({
+                message: "can not add action", error
+            })
+        })
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
     const id = req.params.id;
-    const body = req.body;
-    actions.update(id, body)
-    .then(action => {
-        res.status(200),json(action)
-    })
-    .catch(error => {
-        res.status(404).json({
-            errorMessage: "Can not Update Action", error
+    const data = req.body;
+    actions.update(id, data)
+        .then(action => {
+            res.status(200).json({
+                Messsage: "Action Succesfully Updated", action
+            });
         })
-    })
+        .catch(error => {
+            res.status(404).json({
+                errorMessage: "Can not Update Action", error
+            })
+        })
 });
 
 router.delete("/:id", (req, res) => {
     actions.remove(req.params.id)
-    .then(action => {
-        res.status(200).json(action)
-    })
-    .catch(error => {
-        res.status(500).json({
-            errorMessage:`could not delete action by id`, error
+        .then(action => {
+            res.status(200).json(action)
         })
-    })
+        .catch(error => {
+            res.status(500).json({
+                errorMessage: `could not delete action by id`, error
+            })
+        })
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -12,9 +12,12 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+require('dotenv').config(); 
 
 const server = require('./server.js');
 
-server.listen(4000, () => {
-    console.log(`\n === Server Running on http://localhost:4000 ===\n`);
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\n === Server Running on http://localhost:${port} ===\n`);
 });
