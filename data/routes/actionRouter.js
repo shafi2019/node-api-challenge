b61390458bd5502aa47dba28e9bb66d485aff42f
Shafi Masoumi <masoumi2017@icloud.com>
2020-01-28 13:36:10 -0800

    latest updates

--- data/routes/actionRouter.js
+++ data/routes/actionRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const actions = require('../helpers/actionModel.js');
+const actionsDb = require('../helpers/actionModel.js');
+const projects = require('../helpers/projectModel.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    actions.get()
+    actionsDb.get()
         .then(data => {
             res.status(200).json(data)
         })
@@ -15,22 +16,74 @@ router.get('/', (req, res) => {
 })
 
 
-router.post('/:id', (req, res) => {
-    actions.insert(req.id)
+
+/*
+router.post("/actions/:id", (req, res) => {
+    const body = req.body;
+    const id = req.params.id;
+    const newAction = { ...body, project_id: id }
+
+    actions.insert(newAction)
         .then(action => {
-            res.status(201).json(action);
+            console.log(action)
+            if (!action) {
+                res.status(400).json({
+                    message: "missing action data"
+                });
+            } else if (!action.description || !action.notes) {
+                res.status(400).json({
+                    message: "missing required description and notes fields"
+                });
+            } else {
+                res.status(200).json({
+                    Message: "Action Successfully added", action
+                });
+            }
         })
-        .catch(error => {
-            res.status(404).json({
-                message: "can not add action", error
-            })
-        })
+        .catch(err => {
+            res.status(500).json({
+                errorMessage: `There was an error while saving the action ${err.res}`
+            });
+        });
+});
+*/
+
+router.post('/:id', validateProjectId, (req, res) => {
+    console.log(req.body)
+    const newAction = { description: req.body.description, notes: req.body.notes, project_id: req.params.id }
+    console.log(newAction)
+    actionsDb.insert(newAction)
+    .then(action =>{
+        res.status(201).json(action);
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'Error adding the action', err
+        });
+    });
 });
 
+/*
+router.post("/:id", (req, res) => {
+    Actions.insert(req.id)
+        .then(action => {
+            console.log(action)
+            res.status(201).json({
+                Message: 'Action Succesfully Added', action
+            });
+        })
+        .catch(error => {
+            console.log(error)
+            res.status(404).json({
+                message: "can not add action", error
+            });
+        });
+});
+*/
 router.put("/:id", (req, res) => {
     const id = req.params.id;
     const data = req.body;
-    actions.update(id, data)
+    actionsDb.update(id, data)
         .then(action => {
             res.status(200).json({
                 Messsage: "Action Succesfully Updated", action
@@ -44,7 +97,7 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
-    actions.remove(req.params.id)
+    actionsDb.remove(req.params.id)
         .then(action => {
             res.status(200).json(action)
         })
@@ -54,5 +107,25 @@ router.delete("/:id", (req, res) => {
             })
         })
 });
+
+
+// Middleware
+function validateProjectId(req, res, next) {
+    const { id } = req.params;
+
+    projects.get(id)
+        .then(project => {
+            if (project) {
+                req.project = project;
+                next();
+            } else {
+                res.status(400).json({ message: 'Error adding the action' });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: err });
+        });
+};
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const server = express();
 server.use(express.json());
 server.use(logger);
 
-const actionRouter = require('./data/routes/actionRouter.js');
-const projectRouter = require('./data/routes/projectRouter.js');
+const actionRouter = require("./data/routes/actionRouter.js");
+const projectRouter = require("./data/routes/projectRouter.js");
 
-server.use('/api/actions', actionRouter);
-server.use('/api/projects', projectRouter)
+server.use("/api/actions", actionRouter);
+server.use("/api/projects", projectRouter)
 
 
 server.get('/', (req, res) => {
@@ -18,12 +18,8 @@ server.get('/', (req, res) => {
 
 
 function logger(req, res, next) {
-    console.log(
-      `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
-        'Origin'
-      )}`
-    );
-    next();
-  };
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+  next();
+}
 
 module.exports = server;
